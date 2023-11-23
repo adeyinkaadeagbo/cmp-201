47cccc5a5fe262712c3e19d499667449d6d3c296
adeyinkaadeagbo <130254148+adeyinkaadeagbo@users.noreply.github.com>
2023-11-23 09:18:48 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    double salary;
+    float taxrate;
+
+    age = 16;
+    printf ( "Age: 9od", age);
+    scanf ( "9od", &age);
+    printf ( "Age: 9od, age");
+    return 0;
+}
