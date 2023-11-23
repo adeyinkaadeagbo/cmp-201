26ea4bd2052f47cd76d45abfee9aafaacf7607cc
adeyinkaadeagbo <130254148+adeyinkaadeagbo@users.noreply.github.com>
2023-11-23 09:20:16 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int age;
-    double salary;
-    float taxrate;
-
-    age = 16;
-    printf ( "Age: 9od", age);
-    scanf ( "9od", &age);
-    printf ( "Age: 9od, age");
+int main () {
+    int number;
+    float average;
+    int number2;
+    number = 20;
+    number2 = 30;
+    printf("the value of number is: %d", number);
+    printf("\nthe value of number2 is: %d", number2);
+    average = (number + number2)/2;
+    printf ("\nthevalue of average is: %f", average);
     return 0;
 }
